83bf124bd99b5086d05df0f3b38662fe6c2aefc5
student <student@pop-os.localdomain>
2023-11-10 11:08:08 +1300

    Added background, media folder, edited layout

--- client/components/HeaderComp.tsx
+++ client/components/HeaderComp.tsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react'
 import { getGreeting } from '../apiClient.ts'
 import { Link } from 'react-router-dom'
 
-function HomePage() {
+function Header() {
   return (
     <>
+      <div className="header">
+        <h1>Weather 4 Worms</h1>
+      </div>
     </>
   )
 }
 
-export default HomePage
+export default Header
