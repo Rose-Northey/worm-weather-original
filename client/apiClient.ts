7c76195ca34096bbbf49aeb58026db4cb92d21e5
Matthew Ranford <matt.ranford16@gmail.com>
2023-11-09 20:53:17 +1300

    Initial file structure setup

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,5 +1,9 @@
 import request from 'superagent'
 
+// import WeatherData from './models/Weather.ts'
+
+// const serverURL =
+
 export async function getGreeting(): Promise<string> {
   const res = await request.get('/api/v1/greeting')
   return res.body.greeting
